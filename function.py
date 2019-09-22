53d0cdd707597acdbcbc8e2f935e274774e582a4
fleyck <34206203+fleyck@users.noreply.github.com>
2019-09-22 22:19:14 +0500

    shlyapa

--- function.py
+++ function.py
@@ -1,1 +1,5 @@
-
+    u_input = int(input("Введите число смещения: "))
+    let_input = input("Введите слово: ")
+    letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u",
+               "v", "w", "x", "y", "z"]
+    #if u_input == '2'
