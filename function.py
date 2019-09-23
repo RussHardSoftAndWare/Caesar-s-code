a2e693756ed4c122859210d3f584f5d37075ed38
fleyck <34206203+fleyck@users.noreply.github.com>
2019-09-24 00:08:44 +0500

    Update function.py

--- function.py
+++ function.py
@@ -1,7 +1,9 @@
 u_input = int(input("Введите число смещения: "))
 let_input = input("Введите слово: ")
 letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u",
-"v", "w", "x", "y", "z",]
+"v", "w", "x", "y", "z"]
+lettersru = ["а", "б", "в", "г", "д", "е", "ё", "ж", "з", "и", "й", "к", "л", "м", "н", "о", "п", "р", "с", "т",
+"у", "ф", "х", "ц", "ч","ш", "щ", "ъ", "ы", "ь", "э", "ю", "я"]
 word = []
 word_after = ""
 for z in let_input:
@@ -10,6 +12,10 @@ for z in word:
     if z == " ":
         word_after = word_after + " "
     else:
-        x = letters.index(z) - u_input
-        word_after = word_after + letters[x]
+        if z in letters:
+            x = letters.index(z) - u_input
+            word_after = word_after + letters[x]
+        elif z in lettersru:
+            x = lettersru.index(z) - u_input
+            word_after = word_after + lettersru[x]
 print(word_after)
